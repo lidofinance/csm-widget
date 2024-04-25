8c61c64c9031ecb86c598f61771e7057ebadef0c
exromany <exromany@lido.fi>
2024-04-25 08:39:13 +0700

    feat: fetch nos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,24 @@
+import { DashboardPage } from 'features/dashboard';
 import { WelcomePage } from 'features/welcome';
+import {
+  useNodeOperatorId,
+  useNodeOperatorLoaded,
+} from 'providers/node-operator-provider';
 // import { DashboardPage } from 'features/dashboard';
 
 // export default () => <DashboardPage fallback={WelcomePage} />;
-export default WelcomePage;
+
+const Switch = () => {
+  const no = useNodeOperatorId();
+  const loading = useNodeOperatorLoaded();
+
+  return (
+    <>
+      {loading && <div>...loading</div>}
+      {!loading && no && <DashboardPage />}
+      {!loading && !no && <WelcomePage />}
+    </>
+  );
+};
+
+export default Switch;
--- providers/node-operator-provider.tsx
+++ providers/node-operator-provider.tsx
@@ -1,6 +1,10 @@
 import { useContractSWR } from '@lido-sdk/react';
 import { BigNumber } from 'ethers';
-import { NodeOperatorAddedEvent } from 'generated/CSModule';
+import {
+  NodeOperatorAddedEvent,
+  NodeOperatorManagerAddressChangedEvent,
+  NodeOperatorRewardAddressChangedEvent,
+} from 'generated/CSModule';
 import {
   FC,
   createContext,
@@ -9,11 +13,11 @@ import {
   PropsWithChildren,
 } from 'react';
 import { useCSModuleRPC } from 'shared/hooks/useCSM';
-import { useAccount } from 'wagmi';
+import { Address, useAccount } from 'wagmi';
 
 export type NodeOperatorContextValue = {
-  list: BigNumber[];
-  current?: BigNumber;
+  list: NO[];
+  current?: NO;
   initialLoading: boolean;
 };
 
@@ -22,28 +26,112 @@ export const NodeOperatorContext = createContext<NodeOperatorContextValue>({
   initialLoading: true,
 });
 
-export const NodeOperatorPrivider: FC<PropsWithChildren> = ({ children }) => {
-  const { address } = useAccount();
+type NO = {
+  id: BigNumber;
+} & NOShape;
+
+type NOShape = Partial<{
+  manager: boolean;
+  rewards: boolean;
+}>;
+
+const useNodeOperators = (address?: Address) => {
   const contract = useCSModuleRPC();
 
-  const filter = contract.filters.NodeOperatorAdded(null, null, address);
+  type FilterEvents = Array<
+    | NodeOperatorAddedEvent
+    | NodeOperatorRewardAddressChangedEvent
+    | NodeOperatorManagerAddressChangedEvent
+  >;
+  const filters = useMemo(() => {
+    const createRewardsAddress = contract.filters.NodeOperatorAdded(
+      null,
+      null,
+      address,
+    );
+    // const createManagerAddress = contract.filters.NodeOperatorAdded(
+    //   null,
+    //   address,
+    //   null,
+    // );
+    // const grantRewardsAddress =
+    //   contract.filters.NodeOperatorRewardAddressChanged(null, null, address);
+    // const grantManagerAddress =
+    //   contract.filters.NodeOperatorManagerAddressChanged(null, null, address);
+    // const loseRewardsAddress =
+    //   contract.filters.NodeOperatorRewardAddressChanged(null, address, null);
+    // const loseManagerAddress =
+    //   contract.filters.NodeOperatorManagerAddressChanged(null, address, null);
+    return [
+      createRewardsAddress,
+      // createManagerAddress,
+      // grantRewardsAddress,
+      // grantManagerAddress,
+      // loseRewardsAddress,
+      // loseManagerAddress,
+    ];
+  }, [address, contract.filters]);
+
   const { data, initialLoading } = useContractSWR({
     contract,
     method: 'queryFilter',
-    params: [filter], // @todo: add change address events
+    params: filters,
+    shouldFetch: !!address,
   });
 
-  const value = useMemo(() => {
-    const list =
-      (data as NodeOperatorAddedEvent[] | undefined)?.map(
-        (e) => e.args.nodeOperatorId,
-      ) || [];
-    return {
-      list,
-      current: list?.[0],
-      initialLoading,
+  const nos = useMemo(() => {
+    const nos: NO[] = [];
+
+    const getNo = (id: BigNumber) => nos.find((no) => no.id.eq(id));
+
+    const modifyNO = (id: BigNumber, shape: NOShape, append = false) => {
+      const no = getNo(id);
+      if (no) {
+        Object.assign(no, shape);
+      } else if (append) {
+        nos.push({ id, ...shape });
+      }
     };
-  }, [data, initialLoading]);
+
+    if (!data) return nos;
+    (data as FilterEvents)
+      .sort((a, b) => a.blockNumber - b.blockNumber)
+      .forEach((e) => {
+        const id = e.args.nodeOperatorId;
+        switch (e.event) {
+          case 'NodeOperatorAdded':
+            return modifyNO(id, { manager: true, rewards: true }, true);
+          case 'NodeOperatorRewardAddressChanged':
+            return e.args[2] === address
+              ? modifyNO(id, { rewards: true }, true)
+              : modifyNO(id, { rewards: false });
+          case 'NodeOperatorManagerAddressChanged':
+            return e.args[2] === address
+              ? modifyNO(id, { manager: true }, true)
+              : modifyNO(id, { manager: false });
+          default:
+            return;
+        }
+      });
+    return nos;
+  }, [address, data]);
+
+  return { data: nos, initialLoading };
+};
+
+export const NodeOperatorPrivider: FC<PropsWithChildren> = ({ children }) => {
+  const { address } = useAccount();
+
+  const { data: list, initialLoading } = useNodeOperators(address);
+
+  const value = useMemo(
+    () => ({
+      list,
+      current: list[0] || undefined,
+      initialLoading,
+    }),
+    [list, initialLoading],
+  );
 
   return (
     <NodeOperatorContext.Provider value={value}>
