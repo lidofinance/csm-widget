d3d8595e471acddebe82a1e95fc42fc3807c6da1
exromany <exromany@lido.fi>
2024-05-03 15:01:59 +0300

    feat: unsupported chain fallback

--- features/welcome/welcome-page.tsx
+++ features/welcome/welcome-page.tsx
@@ -2,13 +2,17 @@ import { FC } from 'react';
 
 import { Layout } from 'shared/components';
 import { Banner } from './banner';
+import { useAccount } from 'shared/hooks';
+import { Fallback } from 'shared/hat/fallback/fallback';
 
 export const WelcomePage: FC = () => {
+  const { active, isConnected } = useAccount();
   return (
     <Layout
       title="Community Staking Module"
       subtitle="Your way to solo staking"
     >
+      {!active && isConnected ? <Fallback /> : undefined}
       <Banner />
     </Layout>
   );
