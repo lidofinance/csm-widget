ab81d8b24f2ef8883082b1ce031e18bc54fe91a1
exromany <exromany@lido.fi>
2024-06-19 15:25:31 +0300

    feat: not released

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,5 @@
-import { DashboardPage } from 'features/dashboard';
-import { GateActiveUser, GateLoaded, GateNodeOperator } from 'features/gates';
-import { StarterPackPage } from 'features/starter-pack';
-import { SplashPage, WelcomePage } from 'features/welcome';
+import { WelcomePage } from 'features/welcome';
 
-const Page = () => (
-  <GateLoaded fallback={<SplashPage />}>
-    <GateActiveUser fallback={<WelcomePage />}>
-      <GateNodeOperator fallback={<StarterPackPage />}>
-        <DashboardPage />
-      </GateNodeOperator>
-    </GateActiveUser>
-  </GateLoaded>
-);
+const Page = WelcomePage;
 
 export default Page;
