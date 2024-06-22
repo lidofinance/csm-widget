83443c9a0a668b640f4650807ff0af61b22013e8
exromany <exromany@lido.fi>
2024-06-22 10:12:09 +0300

    fix: typo staking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -56,7 +56,7 @@ export default class MyDocument extends Document {
   }
 
   get metaTitle(): string {
-    return 'Community Stacking Module | Lido';
+    return 'Community Staking Module | Lido';
   }
 
   get metaDescription(): string {
