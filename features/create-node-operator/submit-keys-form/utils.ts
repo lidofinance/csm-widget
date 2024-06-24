de7d25a8ff642d64de3b8cb070375ead9e1f0796
exromany <exromany@lido.fi>
2024-06-24 11:18:54 +0300

    chore: drop unused

--- features/create-node-operator/submit-keys-form/utils.ts
+++ features/create-node-operator/submit-keys-form/utils.ts
@@ -1,31 +1,5 @@
-import type { BaseProvider } from '@ethersproject/providers';
 import { ContractReceipt } from 'ethers';
-import { isAddress } from 'ethers/lib/utils';
-
 import { NodeOperatorAddedEvent } from 'generated/CSModule';
-
-export const getAddress = async (
-  input: string,
-  providerRpc: BaseProvider,
-): Promise<string> => {
-  try {
-    if (isAddress(input)) return input;
-    const address = await providerRpc.resolveName(input);
-    if (address) return address;
-  } catch {
-    // noop
-  }
-  throw new ReferralAddressError();
-};
-
-export class ReferralAddressError extends Error {
-  reason: string;
-  constructor() {
-    const message = 'execution reverted: INVALID_REFERRAL';
-    super(message);
-    this.reason = message;
-  }
-}
 
 export const getAddedNodeOperator = (receipt: ContractReceipt) => {
   const event = receipt.events?.find(
