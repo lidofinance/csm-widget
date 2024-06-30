edf3e998f286183dcbf082df524974928ccdcff1
exromany <exromany@lido.fi>
2024-06-30 16:49:52 +0300

    fix: devnet vs testnet

--- consts/csm-early-adoption.ts
+++ consts/csm-early-adoption.ts
@@ -1,13 +1,15 @@
 import { CHAINS } from '@lido-sdk/constants';
+import { config } from 'config';
 
 export type ChainTreeLinkMap = Partial<Record<CHAINS, string>>;
 
 export const LINKS_BY_NETWORK: ChainTreeLinkMap = {
   [CHAINS.Mainnet]: undefined,
   // @note devnet.1
-  [CHAINS.Holesky]: process.env.DEVNET
-    ? 'https://raw.githubusercontent.com/lidofinance/community-staking-module/main/artifacts/devnet-1/early-adoption/merkle-tree.json'
-    : 'https://raw.githubusercontent.com/lidofinance/community-staking-module/main/artifacts/holesky/early-adoption/merkle-tree.json',
+  [CHAINS.Holesky]:
+    process.env.DEVNET || config.isDevnet
+      ? 'https://raw.githubusercontent.com/lidofinance/community-staking-module/main/artifacts/devnet-1/early-adoption/merkle-tree.json'
+      : 'https://raw.githubusercontent.com/lidofinance/community-staking-module/main/artifacts/holesky/early-adoption/merkle-tree.json',
 };
 
 export const getCSMEarlyAdoptionTreeUrl = (chainId: CHAINS | undefined) => {
