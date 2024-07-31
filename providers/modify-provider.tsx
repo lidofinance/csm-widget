902a105608f6f76d7ae59daa4fef3276e2fd8c2b
exromany <exromany@lido.fi>
2024-07-31 15:26:35 +0300

    fix: drop ?addrs

--- providers/modify-provider.tsx
+++ providers/modify-provider.tsx
@@ -15,7 +15,6 @@ type ModifyContextValue = {
   referrer?: string;
 };
 
-const CUSTOM_ADDRESSES_QUERY = 'addrs';
 const REFERRER_QUERY = 'ref';
 
 const ModifyContext = createContext<ModifyContextValue | null>(null);
@@ -44,9 +43,6 @@ export const ModifyProvider: FC<PropsWithChildren> = ({ children }) => {
   const { query, isReady } = useRouter();
 
   useEffect(() => {
-    if (isReady && query[CUSTOM_ADDRESSES_QUERY] !== undefined) {
-      setCustomAddresses(true);
-    }
     if (isReady && query['mode'] === 'extended') {
       setCustomAddresses(true);
     }
