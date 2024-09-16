adc36a151dd3b4fe7274ae1b15b9735ed3a99587
exromany <exromany@lido.fi>
2024-09-16 10:15:14 +0200

    feat: claim eth modal

--- features/claim-bond/claim-bond-form/hooks/use-tx-modal-stages-claim-bond.tsx
+++ features/claim-bond/claim-bond-form/hooks/use-tx-modal-stages-claim-bond.tsx
@@ -1,10 +1,14 @@
 import type { BigNumber } from 'ethers';
 
 import { TOKENS } from 'consts/tokens';
+import { MatomoLink } from 'shared/components';
+import { TxLinkEtherscan } from 'shared/components/tx-link-etherscan';
 import {
   TransactionModalTransitStage,
+  TxAmount,
   TxStageOperationSucceedBalanceShown,
   TxStageSignOperationAmount,
+  TxStageSuccess,
   getGeneralTransactionModalStages,
   useTransactionModalStage,
 } from 'shared/transaction-modal';
@@ -14,6 +18,7 @@ const STAGE_OPERATION_ARGS = {
 };
 
 type Props = { amount: BigNumber; token: TOKENS };
+type SuccessProps = { amount: BigNumber; balance: BigNumber; token: TOKENS };
 
 const getTxModalStagesClaimBond = (
   transitStage: TransactionModalTransitStage,
@@ -46,14 +51,37 @@ const getTxModalStagesClaimBond = (
       />,
     ),
 
-  success: ({ balance }: { balance: BigNumber }, txHash?: string) =>
+  success: ({ amount, balance, token }: SuccessProps, txHash?: string) =>
     transitStage(
-      <TxStageOperationSucceedBalanceShown
-        {...STAGE_OPERATION_ARGS}
-        txHash={txHash}
-        balance={balance}
-        balanceToken={TOKENS.STETH}
-      />,
+      token === TOKENS.ETH ? (
+        <TxStageSuccess
+          title="Withdrawal request has been sent"
+          description={
+            <>
+              Request withdrawal of{' '}
+              <TxAmount amount={amount} token={TOKENS.STETH} /> has been sent.
+              Check{' '}
+              <MatomoLink href="https://stake.lido.fi/claim">
+                Claim tab on the Lido Staking Widget
+              </MatomoLink>{' '}
+              to view your withdrawal requests or view your transaction on{' '}
+              <TxLinkEtherscan txHash={txHash} text="Etherscan" />.
+              <br />
+              <br />
+              Add NFT to your wallet to monitor the status of your request.
+              <MatomoLink href="">This guide</MatomoLink> will help you to do
+              this.
+            </>
+          }
+        />
+      ) : (
+        <TxStageOperationSucceedBalanceShown
+          {...STAGE_OPERATION_ARGS}
+          txHash={txHash}
+          balance={balance}
+          balanceToken={TOKENS.STETH}
+        />
+      ),
       {
         isClosableOnLedger: true,
       },
