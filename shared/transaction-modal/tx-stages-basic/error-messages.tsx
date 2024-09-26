e90bef5454ada86353cc40ad944230f412db4fb0
exromany <exromany@lido.fi>
2024-09-26 11:48:04 +0200

    fix: wording

--- shared/transaction-modal/tx-stages-basic/error-messages.tsx
+++ shared/transaction-modal/tx-stages-basic/error-messages.tsx
@@ -6,20 +6,21 @@ export const ErrorMessages: Record<ErrorCode, ReactNode> = {
   [ErrorCode.DENIED_SIG]: 'User denied the transaction signature.',
   [ErrorCode.SOMETHING_WRONG]: 'Something went wrong.',
   [ErrorCode.TRANSACTION_REVERTED]:
-    'Transaction was included into block but reverted during execution',
+    'Transaction was included into block but reverted during execution.',
   [ErrorCode.ENABLE_BLIND_SIGNING]:
     'Please enable blind signing on your Ledger hardware wallet.',
   [ErrorCode.LIMIT_REACHED]:
     'Transaction could not be completed because stake limit is exhausted. Please wait until the stake limit restores and try again. Otherwise, you can swap your Ethereum on 1inch platform instantly.',
-  [ErrorCode.DEVICE_LOCKED]: 'Please unlock your Ledger hardware wallet',
-  [ErrorCode.INVALID_REFERRAL]: 'Invalid referral address or ENS',
+  [ErrorCode.DEVICE_LOCKED]: 'Please unlock your Ledger hardware wallet.',
+  [ErrorCode.INVALID_REFERRAL]: 'Invalid referral address or ENS.',
   [ErrorCode.INVALID_SIGNATURE]:
-    'Invalid Permit signature. Perhaps it has expired or already been used. Try submitting a withdrawal request again.',
-  [ErrorCode.BALANCE_EXCEEDED]: 'Balance exceeded',
+    'Invalid Permit signature. Perhaps it has expired or already been used. Try submitting the request again.',
+  [ErrorCode.BALANCE_EXCEEDED]:
+    'Balance exceeded. Not enough tokens to perform TX.',
   [ErrorCode.WALLET_RPC]: (
     <>
       Wallet RPC provider returns &quot;Too many requests&quot;. <br />
-      Try to change RPC url in your wallet
+      Try to change RPC url in your wallet.
     </>
   ),
 };
