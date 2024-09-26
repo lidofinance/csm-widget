1768918c74de0f687779ac45422880ebc6959c6b
exromany <exromany@lido.fi>
2024-09-26 11:39:30 +0200

    fix: wording

--- shared/hook-form/controls/paused-button.tsx
+++ shared/hook-form/controls/paused-button.tsx
@@ -10,7 +10,8 @@ const titles: Record<PauseType, string> = {
 };
 
 const hints: Record<PauseType, ReactNode> = {
-  Module: 'This means that claim and add bond is currently not possible',
+  Module:
+    'This means that Node Operator creation and keys upload is not possible',
   Accounting: 'This means that claim and add bond is currently not possible',
 };
 
