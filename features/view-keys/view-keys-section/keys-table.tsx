7947f6152d95a94eb8e2dd974ef0265e22c4d59d
exromany <exromany@lido.fi>
2024-09-28 18:31:55 +0200

    fix: view keys: show keys only after loaded statuses

--- features/view-keys/view-keys-section/keys-table.tsx
+++ features/view-keys/view-keys-section/keys-table.tsx
@@ -1,52 +1,47 @@
 import { Address, Tbody, Td, Text, Th, Thead, Tr } from '@lidofinance/lido-ui';
 import { FC } from 'react';
 import { KeyLink, Stack, StatusChip } from 'shared/components';
-import { HexString } from 'shared/keys';
+import { KeyWithStatus } from 'shared/hooks';
 import { AddressRow, TableStyle } from './styles';
-import { useGetKeyStatus } from 'shared/hooks';
 
 type Props = {
-  keys?: HexString[];
+  data?: KeyWithStatus[];
   offset?: number;
 };
 
-export const KeysTable: FC<Props> = ({ keys, offset = 0 }) => {
-  const { data: getStatus } = useGetKeyStatus();
+export const KeysTable: FC<Props> = ({ data, offset = 0 }) => (
+  <TableStyle>
+    <Thead>
+      <Tr>
+        <Th>#</Th>
+        <Th>Key</Th>
+        <Th>Status</Th>
+      </Tr>
+    </Thead>
 
-  return (
-    <TableStyle>
-      <Thead>
-        <Tr>
-          <Th>#</Th>
-          <Th>Key</Th>
-          <Th>Status</Th>
+    <Tbody>
+      {data?.map(({ key, statuses }, index) => (
+        <Tr key={offset + index}>
+          <Td>
+            <Text size="xxs" color="secondary">
+              {offset + index + 1}
+            </Text>
+          </Td>
+          <Td>
+            <AddressRow>
+              <Address address={key} symbols={16} />
+              <KeyLink pubkey={key} />
+            </AddressRow>
+          </Td>
+          <Td>
+            <Stack direction="column" gap="xs">
+              {statuses.map((status) => (
+                <StatusChip status={status} key={status} />
+              ))}
+            </Stack>
+          </Td>
         </Tr>
-      </Thead>
-
-      <Tbody>
-        {keys?.map((key, index) => (
-          <Tr key={offset + index}>
-            <Td>
-              <Text size="xxs" color="secondary">
-                {offset + index + 1}
-              </Text>
-            </Td>
-            <Td>
-              <AddressRow>
-                <Address address={key} symbols={16} />
-                <KeyLink pubkey={key} />
-              </AddressRow>
-            </Td>
-            <Td>
-              <Stack direction="column" gap="xs">
-                {getStatus?.(key, offset + index).map((status) => (
-                  <StatusChip status={status} key={status} />
-                ))}
-              </Stack>
-            </Td>
-          </Tr>
-        ))}
-      </Tbody>
-    </TableStyle>
-  );
-};
+      ))}
+    </Tbody>
+  </TableStyle>
+);
--- shared/hooks/use-keys-with-status.ts
+++ shared/hooks/use-keys-with-status.ts
@@ -3,6 +3,13 @@ import { useMemo } from 'react';
 import { useGetKeyStatus } from './use-get-key-status';
 import { useMergeSwr } from './useMergeSwr';
 import { useNodeOperatorKeys } from './useNodeOperatorKeys';
+import { HexString } from '@openzeppelin/merkle-tree/dist/bytes';
+import { KeyStatus } from 'types';
+
+export type KeyWithStatus = {
+  key: HexString;
+  statuses: KeyStatus[];
+};
 
 export const useKeysWithStatus = () => {
   const nodeOperatorId = useNodeOperatorId();
@@ -25,10 +32,13 @@ export const useKeysWithStatus = () => {
         return undefined;
       }
 
-      return keys.map((key, index) => ({
-        key,
-        statuses: getStatus(key, index),
-      }));
+      return keys.map(
+        (key, index) =>
+          ({
+            key,
+            statuses: getStatus(key, index),
+          }) as KeyWithStatus,
+      );
     }, [getStatus, keys, swrGetStatus.initialLoading, swrKeys.initialLoading]),
   );
 };
