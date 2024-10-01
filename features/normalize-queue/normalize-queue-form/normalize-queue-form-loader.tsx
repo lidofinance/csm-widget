21d64ce9afecabbe9050e4cbab2990cf4403fd62
exromany <exromany@lido.fi>
2024-10-01 11:12:11 +0200

    fix: everyone can normalize queue

--- features/normalize-queue/normalize-queue-form/normalize-queue-form-loader.tsx
+++ features/normalize-queue/normalize-queue-form/normalize-queue-form-loader.tsx
@@ -2,19 +2,11 @@ import { FC, PropsWithChildren } from 'react';
 import { useFormState } from 'react-hook-form';
 import { WhenLoaded } from 'shared/components';
 import { NormalizeQueueFormInputType } from './context';
-import { useActiveNodeOperator } from 'providers/node-operator-provider';
-import { ROLES } from 'consts/roles';
-import { Info } from './controls/info';
 
 export const NormalizeQueueFormLoader: FC<PropsWithChildren> = ({
   children,
 }) => {
   const { isLoading } = useFormState<NormalizeQueueFormInputType>();
 
-  const nodeOperator = useActiveNodeOperator();
-  const isView = !nodeOperator?.roles.includes(ROLES.MANAGER);
-
-  return (
-    <WhenLoaded loading={isLoading}>{isView ? <Info /> : children}</WhenLoaded>
-  );
+  return <WhenLoaded loading={isLoading}>{children}</WhenLoaded>;
 };
