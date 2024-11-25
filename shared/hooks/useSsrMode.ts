f982ed5ffdbc63fce6f1011d4a847251543caed7
exromany <exromany@lido.fi>
2024-11-25 18:11:45 +0300

    chore: remove unused hooks, utils

--- shared/hooks/useSsrMode.ts
+++
@@ -1,7 +0,0 @@
-import { useEffect, useState } from 'react';
-
-export const useSsrMode = () => {
-  const [ssrMode, setSsrMode] = useState(true);
-  useEffect(() => setSsrMode(false), []);
-  return ssrMode;
-};
