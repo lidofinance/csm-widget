75f6940e049cf155e4db9493bc91da7d772bc23c
exromany <exromany@lido.fi>
2024-11-25 18:50:48 +0300

    chore: type fixes

--- shared/navigate/navigate.tsx
+++ shared/navigate/navigate.tsx
@@ -10,5 +10,5 @@ export const Navigate: FC<Props> = ({ path, fallback = <SplashPage /> }) => {
 
   useEffect(() => void navigate(path), [navigate, path]);
 
-  return fallback;
+  return <>{fallback}</>;
 };
