fd2f7aa07e59f4125faa1288174777245d3ff178
exromany <exromany@lido.fi>
2025-02-04 10:14:35 +0700

    fix: exhausted limit

--- features/create-node-operator/share-limit-banner/share-limit-banner.tsx
+++ features/create-node-operator/share-limit-banner/share-limit-banner.tsx
@@ -23,6 +23,28 @@ const ReachedBanner: FC = () => (
   </Banner>
 );
 
+const ExhaustedBanner: FC<Props> = ({ activeLeft, queue }) => (
+  <Banner
+    variant="wary-dangerous"
+    title="CSM has exhausted its limit for new keys"
+  >
+    Currently, <b>{activeLeft}</b> more keys can be activated in CSM before it
+    hits its stake share limit. Since there are already <b>{queue}</b> keys in
+    the queue, this means that newly uploaded keys may not receive deposits in
+    the near future. You can still upload keys, but in case CSM reaches the
+    limit before your keys get deposited to, the deposit time for your keys can
+    be months or longer. Please note that the exact number of validators that
+    can be active for CSM is constantly changing due to protocol dynamics.
+    <br />
+    <LocalLink
+      anchor="#stake-share-limit"
+      matomoEvent={MATOMO_CLICK_EVENTS_TYPES.stakeShareLimitLinkBanner}
+    >
+      Read more in the FAQ section
+    </LocalLink>
+  </Banner>
+);
+
 const ApproachingBanner: FC<Props> = ({ activeLeft, queue }) => (
   <Banner variant="wary" title="CSM is approaching its stake share limit">
     Currently, <b>{activeLeft}</b> more keys can be activated in CSM before it
@@ -49,6 +71,11 @@ export const ShareLimitBanner: FC = () => {
     <>
       {data?.status === SHARE_LIMIT_STATUS.REACHED ? (
         <ReachedBanner />
+      ) : data?.status === SHARE_LIMIT_STATUS.EXHAUSTED ? (
+        <ExhaustedBanner
+          activeLeft={data.activeLeft.toString()}
+          queue={data.queue.toString()}
+        />
       ) : data?.status === SHARE_LIMIT_STATUS.APPROACHING ? (
         <ApproachingBanner
           activeLeft={data.activeLeft.toString()}
