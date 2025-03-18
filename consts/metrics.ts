ece5320b4d4111142e6b3c68dc375e4e002fb849
exromany <exromany@lido.fi>
2025-03-18 07:11:10 +0300

    chore: drop unused subgraph code
    chore: drop consts

--- consts/metrics.ts
+++ consts/metrics.ts
@@ -5,6 +5,5 @@ export const METRICS_PREFIX = 'csm_widget_ui_';
 export const enum METRIC_NAMES {
   REQUESTS_TOTAL = 'requests_total',
   API_RESPONSE = 'api_response',
-  SUBGRAPHS_RESPONSE = 'subgraphs_response',
   ETH_CALL_ADDRESS_TO = 'eth_call_address_to',
 }
