7ac35f77dce70ed925ce0cee44183397f58d4a50
exromany <exromany@lido.fi>
2025-03-18 07:11:10 +0300

    chore: drop unused subgraph code
    chore: drop consts

--- utilsApi/index.ts
+++ utilsApi/index.ts
@@ -1,6 +1,5 @@
 export * from './contractAddressesMetricsMap';
 export * from './getEthPrice';
-export * from './getSubgraphUrl';
 export * from './nextApiWrappers';
 export * from './rpcProviders';
 export * from './metrics';
