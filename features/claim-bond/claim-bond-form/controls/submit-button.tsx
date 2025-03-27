fec63347e969299236170b12ce5c4f187adfef98
exromany <exromany@lido.fi>
2025-03-27 18:56:12 +0700

    feat: claim - title on submit button

--- features/claim-bond/claim-bond-form/controls/submit-button.tsx
+++ features/claim-bond/claim-bond-form/controls/submit-button.tsx
@@ -1,22 +1,32 @@
+import { TOKENS } from 'consts/tokens';
+import { useWatch } from 'react-hook-form';
 import { PausedButton, SubmitButtonHookForm } from 'shared/hook-form/controls';
 import { ClaimBondFormInputType, useClaimBondFormData } from '../context';
-import { useWatch } from 'react-hook-form';
 
 export const SubmitButton = () => {
-  const claimRewards = useWatch<ClaimBondFormInputType, 'claimRewards'>({
-    name: 'claimRewards',
+  const [claimRewards, token, amount] = useWatch<
+    ClaimBondFormInputType,
+    ['claimRewards', 'token', 'amount']
+  >({
+    name: ['claimRewards', 'token', 'amount'],
   });
 
-  const { isPaused } = useClaimBondFormData();
+  const { isPaused, maxValues } = useClaimBondFormData();
   if (isPaused) {
     return <PausedButton type="Accounting" />;
   }
 
-  // TODO: disable
-  // TODO: nothing to claim
+  const isNothingToClaim = !!maxValues?.STETH[1]?.isZero();
+  const isPullRewards = !!amount?.isZero() && claimRewards;
+  const text = isNothingToClaim
+    ? 'Nothing to claim'
+    : token === TOKENS.ETH
+      ? 'Request withdrawal to the Rewards Address'
+      : isPullRewards
+        ? 'Claim rewards to the Bond balance'
+        : 'Claim to the Rewards Address';
+
   return (
-    <SubmitButtonHookForm errorField="amount">
-      {claimRewards ? 'Claim Rewards and Bond' : 'Claim Bond'}
-    </SubmitButtonHookForm>
+    <SubmitButtonHookForm errorField="amount">{text}</SubmitButtonHookForm>
   );
 };
