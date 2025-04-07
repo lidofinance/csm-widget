2f7ce521aff7324d9daa9a0b788e7deeb3eb8390
exromany <exromany@lido.fi>
2025-04-07 13:29:24 +0300

    feat: ethseer api proxy

--- features/monitoring/monitoring.tsx
+++ features/monitoring/monitoring.tsx
@@ -1,9 +1,15 @@
 import { FC } from 'react';
 import { ExternalSection } from './external';
+import { AttestationRateSection } from './attestation-rate-section';
+import { getConfig } from 'config';
+import { CHAINS } from 'consts/chains';
+
+const { defaultChain } = getConfig();
 
 export const Monitoring: FC = () => {
   return (
     <>
+      {defaultChain === CHAINS.Mainnet && <AttestationRateSection />}
       <ExternalSection />
     </>
   );
