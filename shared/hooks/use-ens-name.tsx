2589e20998c084375b4d5411e0cd92988b37b767
exromany <exromany@lido.fi>
2025-05-05 17:06:13 +0200

    chore: cache ens name

--- shared/hooks/use-ens-name.tsx
+++ shared/hooks/use-ens-name.tsx
@@ -1,14 +1,29 @@
-import { useLidoSWR, useSDK } from '@lido-sdk/react';
+import { useLidoSWR, useLocalStorage, useSDK } from '@lido-sdk/react';
 import { STRATEGY_IMMUTABLE } from 'consts/swr-strategies';
 import { Address } from 'wagmi';
+import { useMergeSwr } from './useMergeSwr';
+import { useEffect } from 'react';
 
 export const useEnsName = (address?: Address) => {
+  const [storedValue, setStoredValue] = useLocalStorage<string | null>(
+    `ens-${address}`,
+    null,
+  );
   const { providerRpc } = useSDK();
-  return useLidoSWR(
+
+  const swrEns = useLidoSWR(
     ['ens-name', address],
     address ? () => providerRpc.lookupAddress(address) : null,
     STRATEGY_IMMUTABLE,
   );
+
+  useEffect(() => {
+    if (swrEns.data !== undefined) {
+      setStoredValue(swrEns.data);
+    }
+  }, [setStoredValue, swrEns.data]);
+
+  return useMergeSwr([swrEns], storedValue);
 };
 
 export const useEnsAvatar = (name?: string) => {
