6fb8bfdd489a7fa5375fee061c4e7f7fec05900d
exromany <exromany@lido.fi>
2025-05-19 13:30:25 +0300

    chore: update text

--- shared/navigate/inner-pages/unsupported-page.tsx
+++ shared/navigate/inner-pages/unsupported-page.tsx
@@ -7,13 +7,10 @@ import { Layout } from 'shared/layout';
 export const UnsupportedPage: FC = () => {
   return (
     <Layout>
-      <Banner
-        title="CSM is currently undergoing an upgrade"
-        variant="secondary"
-      >
+      <Banner title="CSM is currently being upgraded" variant="secondary">
         <Text size="sm">
-          CSM v2 is on the way, packed with new benefits! A new version of the
-          widget will be available shortly to enhance your experience
+          CSM v2 is on the way — packed with new benefits! A new version of the
+          widget will be available soon.
         </Text>
       </Banner>
       <WelcomeSection></WelcomeSection>
