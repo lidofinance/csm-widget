b9ebec5e31cc3cd9a97733d7f7ff5181906c2a2c
Aleksandr <captain_sparrow@lido.fi>
2025-06-17 11:39:08 -0300

    fix: try to fix test

--- tests/pages/elements/keys/element.createNodeOperatorForm.ts
+++ tests/pages/elements/keys/element.createNodeOperatorForm.ts
@@ -1,4 +1,4 @@
-import { Locator, Page, expect } from '@playwright/test';
+import { Locator, Page, expect, test } from '@playwright/test';
 import {
   EXTRA_PER_KEY,
   FIRST_BOND,
@@ -38,29 +38,33 @@ export class CreateNodeOperatorForm {
   }
 
   async fillKeys(keys: DepositKey[]) {
-    const value = JSON.stringify(keys);
-    await this.rawDepositData.fill(value);
+    await test.step('Fill deposit key data', async () => {
+      const value = JSON.stringify(keys);
+      await this.rawDepositData.fill(value);
+    });
   }
 
   async addNewKeys(keys: DepositKey[], tokenSymbol: TokenSymbol) {
-    const bondTokenElement = this.getBondTokenElement(tokenSymbol);
-    await bondTokenElement.click();
-    await this.fillKeys(keys);
-    // This formula follows the Bond Curve model:
-    // https://operatorportal.lido.fi/modules/community-staking-module#block-2d1c307d95fc4f8ab7c32b7584f795cf
-    // The bond for the first key should cost 2.4 ETH, and each subsequent key should cost 1.3 ETH.
-    const expectedBond = FIRST_BOND + (keys.length - 1) * EXTRA_PER_KEY;
-    await expect(
-      this.amountInput,
-      `Expected bond amount will be equal ${expectedBond.toFixed(1)}`,
-    ).toHaveValue(expectedBond.toFixed(1));
-    await this.confirmKeysReady.click();
+    return test.step('Add new keys', async () => {
+      const bondTokenElement = this.getBondTokenElement(tokenSymbol);
+      await bondTokenElement.click();
+      await this.fillKeys(keys);
+      // This formula follows the Bond Curve model:
+      // https://operatorportal.lido.fi/modules/community-staking-module#block-2d1c307d95fc4f8ab7c32b7584f795cf
+      // The bond for the first key should cost 2.4 ETH, and each subsequent key should cost 1.3 ETH.
+      const expectedBond = FIRST_BOND + (keys.length - 1) * EXTRA_PER_KEY;
+      await expect(
+        this.amountInput,
+        `Expected bond amount will be equal ${expectedBond.toFixed(1)}`,
+      ).toHaveValue(expectedBond.toFixed(1));
+      await this.confirmKeysReady.click();
 
-    const [walletSignPage] = await Promise.all([
-      this.base.waitForPage(WALLET_PAGE_TIMEOUT_WAITER),
-      this.submitKeysButton.click(),
-    ]);
+      const [walletSignPage] = await Promise.all([
+        this.base.waitForPage(WALLET_PAGE_TIMEOUT_WAITER),
+        this.submitKeysButton.click(),
+      ]);
 
-    return walletSignPage;
+      return walletSignPage;
+    });
   }
 }
