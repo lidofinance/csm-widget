60b5a7ebd0622c13db2243225087fb0bf4bab8f0
Aleksandr <captain_sparrow@lido.fi>
2025-06-18 08:06:31 -0300

    fix: comments

--- tests/pages/elements/dashboard/bondReward/element.baseSection.ts
+++ tests/pages/elements/dashboard/bondReward/element.baseSection.ts
@@ -1,6 +1,7 @@
 import { Locator, test } from '@playwright/test';
 
 export abstract class BaseExpandedBlock {
+  sectionTestId: string;
   expandedBlock: Locator;
   expandedButton: Locator;
   commonBalance: Locator;
@@ -10,7 +11,8 @@ export abstract class BaseExpandedBlock {
   public abstract waitForExpanded(): Promise<void>;
 
   constructor(section: Locator, sectionTestId: string) {
-    this.expandedBlock = section.getByTestId(sectionTestId);
+    this.sectionTestId = sectionTestId;
+    this.expandedBlock = section.getByTestId(this.sectionTestId);
     this.expandedButton = this.expandedBlock.getByRole('button');
 
     // Common Balance
@@ -25,14 +27,13 @@ export abstract class BaseExpandedBlock {
       (await this.expandedButton.getAttribute('aria-expanded')) === 'true';
 
     if (isExpanded) {
-      console.info(`Section already expanded`);
+      console.info(`Section ${this.sectionTestId} already expanded`);
       return;
     }
 
     const attempts = 5;
-
-    for (let attempt = 1; attempt <= attempts && !isExpanded; attempt++) {
-      await test.step(`Try to expand the "Available to claim" section. (Attempt: ${attempt})`, async () => {
+    await test.step(`Expand the "${this.sectionTestId}" section.`, async () => {
+      for (let attempt = 1; attempt <= attempts && !isExpanded; attempt++) {
         try {
           await this.expandedButton.click();
           await this.waitForExpanded();
@@ -42,11 +43,11 @@ export abstract class BaseExpandedBlock {
           console.warn(`Waiting for expanded failed.`);
           if (attempt === attempts) {
             throw new Error(
-              'Failed to expand "Available to claim" section after 5 attempts',
+              `Failed to expand "${this.sectionTestId}" section after 5 attempts`,
             );
           }
         }
-      });
-    }
+      }
+    });
   }
 }
