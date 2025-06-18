a686f771f06de84efdafe0c3289c1873e1a7deb2
exromany <exromany@lido.fi>
2025-06-18 15:43:22 +0300

    chore: enable surveys cta from 23 Jun to 06 Jul

--- shared/hooks/use-surveys-call.ts
+++ shared/hooks/use-surveys-call.ts
@@ -1,11 +1,16 @@
 import { getConfig } from 'config';
 import { CHAINS } from 'consts/chains';
-import { isBefore, parseISO } from 'date-fns';
+import { isAfter, isBefore, parseISO } from 'date-fns';
 
 const { defaultChain } = getConfig();
 
 export const useSurveysCall = () => {
-  const endOfSurvey = parseISO('2025-04-01T00:00Z');
+  const startOfSurvey = parseISO('2025-06-23T00:00Z');
+  const endOfSurvey = parseISO('2025-07-06T00:00Z');
   const today = new Date();
-  return defaultChain === CHAINS.Mainnet && isBefore(today, endOfSurvey);
+  return (
+    defaultChain === CHAINS.Mainnet &&
+    isAfter(today, startOfSurvey) &&
+    isBefore(today, endOfSurvey)
+  );
 };
