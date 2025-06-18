6c5696ca9ba019ab2b304d0096664549b33bccb8
exromany <exromany@lido.fi>
2025-06-18 15:45:12 +0300

    chore: surveys banner before 6 Jul

--- shared/hooks/use-surveys-call.ts
+++ shared/hooks/use-surveys-call.ts
@@ -1,16 +1,7 @@
-import { getConfig } from 'config';
-import { CHAINS } from 'consts/chains';
-import { isAfter, isBefore, parseISO } from 'date-fns';
-
-const { defaultChain } = getConfig();
+import { isBefore, parseISO } from 'date-fns';
 
 export const useSurveysCall = () => {
-  const startOfSurvey = parseISO('2025-06-23T00:00Z');
   const endOfSurvey = parseISO('2025-07-06T00:00Z');
   const today = new Date();
-  return (
-    defaultChain === CHAINS.Mainnet &&
-    isAfter(today, startOfSurvey) &&
-    isBefore(today, endOfSurvey)
-  );
+  return isBefore(today, endOfSurvey);
 };
