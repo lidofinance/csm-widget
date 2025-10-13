8a5a24dd1f8ad6f8da10d9c98b8c85d6c431c053
Alexandr Vorobey <sashavorobey95@gmail.com>
2025-10-13 20:31:34 +0300

    fix: update report configuration and modify test assertions (#339)

--- tests/widget/operatorWithoutValidator/common.spec.ts
+++ tests/widget/operatorWithoutValidator/common.spec.ts
@@ -8,7 +8,8 @@ import { qase } from 'playwright-qase-reporter/playwright';
 
 test.use({ secretPhrase: process.env.EMPTY_SECRET_PHRASE });
 
-test.describe('Operator without keys. Common suite.', async () => {
+// @TODO: Should fix by QA-4217
+test.describe.skip('Operator without keys. Common suite.', async () => {
   let mainPage: MainPage;
   let createKeysPage: KeysPage;
 
