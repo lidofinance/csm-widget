11376fe3530c8c4483b1aea95d2c012e6921b1c2
exromany <exromany@lido.fi>
2025-10-13 13:15:02 +0300

    chore: update texts & add form note

--- features/transfer-keys/transfer-keys/confirm-cleanup-modal.tsx
+++ features/transfer-keys/transfer-keys/confirm-cleanup-modal.tsx
@@ -10,12 +10,28 @@ export const ConfirmCleanupModal: ModalComponentType<ConfirmModalProps> = ({
   ...props
 }) => {
   return (
-    <Modal {...props} center onClose={onReject}>
+    <Modal {...props} onClose={onReject}>
       <Stack direction="column" gap="xxl">
-        <Text size="xs" color="secondary">
-          To ensure correct positioning of the new keys in the priority queue,
-          you will need to perform two transactions: Transfer keys and Cleanup.
-        </Text>
+        <Stack direction="column" gap="sm">
+          <Text as="h5" size="sm" weight={700}>
+            Two transactions required
+          </Text>
+          <Text size="xs" color="secondary">
+            To ensure correct positioning of the new keys in the priority queue,
+            you will need to perform two transactions:
+          </Text>
+          <Text size="xs" color="secondary">
+            <ol>
+              <li>
+                First transaction will transfer your keys to the priority queue
+              </li>
+              <li>
+                Second transaction will cleanup the queue to maintain correct
+                priority for your new keys
+              </li>
+            </ol>
+          </Text>
+        </Stack>
 
         <Button fullwidth onClick={onConfirm}>
           Continue
