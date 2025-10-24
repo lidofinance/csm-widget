8223f13ab2c30435c873f77008893ed774c9f736
Aleksandr <captain_sparrow@lido.fi>
2025-10-24 23:15:58 +0300

    fix: add GitHub Actions workflow for testing CSM-widget with Fork

--- modules/web3/web3-provider/use-web3-transport.ts
+++ modules/web3/web3-provider/use-web3-transport.ts
@@ -165,19 +165,19 @@ export const useWeb3Transport = (
 
   const onActiveConnection = useCallback(
     async (activeConnection: Connection | null) => {
-      // for (const chain of supportedChains) {
-      // const setTransport = setTransportMap[chain.id];
-      // if (
-      //   activeConnection &&
-      //   chain.id === activeConnection.chainId &&
-      //   activeConnection.connector.type === 'injected'
-      // ) {
-      //   const provider = (await activeConnection.connector?.getProvider?.({
-      //     chainId: chain.id,
-      //   })) as EIP1193Provider | undefined;
-      //   setTransport(provider ? custom(provider) : null);
-      // } else setTransport(null);
-      // }
+      for (const chain of supportedChains) {
+        const setTransport = setTransportMap[chain.id];
+        if (
+          activeConnection &&
+          chain.id === activeConnection.chainId &&
+          activeConnection.connector.type === 'injected'
+        ) {
+          const provider = (await activeConnection.connector?.getProvider?.({
+            chainId: chain.id,
+          })) as EIP1193Provider | undefined;
+          setTransport(provider ? custom(provider) : null);
+        } else setTransport(null);
+      }
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [setTransportMap, supportedChains],
