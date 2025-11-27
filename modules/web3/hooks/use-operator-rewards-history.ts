16e26fce494f1f04cc3cd1e300a1c63de7941cb7
exromany <exromany@lido.fi>
2025-11-27 18:23:02 +0300

    fix: use current rate on testnet

--- modules/web3/hooks/use-operator-rewards-history.ts
+++ modules/web3/hooks/use-operator-rewards-history.ts
@@ -6,6 +6,8 @@ import { useQuery } from '@tanstack/react-query';
 import { STRATEGY_CONSTANT } from 'consts';
 import invariant from 'tiny-invariant';
 import { useLidoSDK } from '../web3-provider';
+import { useDappStatus } from './use-dapp-status';
+import { CHAINS } from '@lidofinance/lido-ethereum-sdk';
 
 export const KEY_OPERATOR_REWARDS_HISTORY = ['operator-rewards-history'];
 
@@ -14,13 +16,17 @@ export const useOperatorRewardsHistory = <TData = OperatorRewardsHistory>(
   select?: (data: OperatorRewardsHistory) => TData,
 ) => {
   const { csm } = useLidoSDK();
+  const { chainId } = useDappStatus();
+  const isTestnet = chainId !== CHAINS.Mainnet;
 
   return useQuery({
     queryKey: [...KEY_OPERATOR_REWARDS_HISTORY, { nodeOperatorId }],
     ...STRATEGY_CONSTANT,
     queryFn: async () => {
       invariant(nodeOperatorId !== undefined);
-      return csm.rewards.getOperatorRewardsHistory(nodeOperatorId);
+      return csm.rewards.getOperatorRewardsHistory(nodeOperatorId, {
+        useCurrentRate: isTestnet,
+      });
     },
     enabled: nodeOperatorId !== undefined,
     select,
