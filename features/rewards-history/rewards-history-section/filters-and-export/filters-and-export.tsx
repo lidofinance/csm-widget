904cf1d3cdbc8c62f502f11b050470f4a79856aa
Roman <exromany@lido.fi>
2025-12-01 18:05:09 +0400

    fix: use current rate on testnet (#382)
    * fix: use current rate on testnet
    
    * fix: view-keys table empty comment
    
    * fix: up sdk for fee in v1 reports
    
    * fix: track click on export button

--- features/rewards-history/rewards-history-section/filters-and-export/filters-and-export.tsx
+++ features/rewards-history/rewards-history-section/filters-and-export/filters-and-export.tsx
@@ -1,5 +1,7 @@
 import { ButtonIcon } from '@lidofinance/lido-ui';
-import { FC } from 'react';
+import { MATOMO_CLICK_EVENTS_TYPES } from 'consts/matomo-click-events';
+import { FC, useCallback } from 'react';
+import { trackMatomoEvent } from 'utils';
 import { SectionWrapper } from './styles';
 import { useRewardsHistoryExport } from './use-rewards-history-export';
 
@@ -8,13 +10,18 @@ import { ReactComponent as DownloadIcon } from 'assets/icons/download.svg';
 export const FiltersAndExport: FC = () => {
   const { exportToCsv, isExporting } = useRewardsHistoryExport();
 
+  const handleExport = useCallback(() => {
+    trackMatomoEvent(MATOMO_CLICK_EVENTS_TYPES.rewardsHistoryExport);
+    void exportToCsv();
+  }, [exportToCsv]);
+
   return (
     <SectionWrapper>
       <ButtonIcon
         icon={<DownloadIcon />}
         size="xs"
         variant="outlined"
-        onClick={exportToCsv}
+        onClick={handleExport}
         loading={isExporting}
       >
         Export all to CSV
