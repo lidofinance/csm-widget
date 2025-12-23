961212dd8d7d31678a38e158ae414d02705c8229
Roman <exromany@lido.fi>
2025-12-23 15:43:16 +0400

    fix: next ics round (#409)

--- features/ics/shared/consts.tsx
+++ features/ics/shared/consts.tsx
@@ -1,1 +1,3 @@
-export const ICS_ASSESSED_DATE = <>October&nbsp;1st</>;
+export const ICS_ROUND = 3;
+export const ICS_ROUND_START_DATE = `December\u00A022, 2025`;
+export const ICS_ASSESSED_DATE = `Q1-Q2\u00A02026`;
