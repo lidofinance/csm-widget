b53f4fe9b931687b84ee0c7f355227c26e7e7160
exromany <exromany@lido.fi>
2025-12-23 10:51:46 +0300

    fix: next ics round

--- features/ics/round-banner/round-banner.tsx
+++ features/ics/round-banner/round-banner.tsx
@@ -2,17 +2,22 @@ import { Text } from '@lidofinance/lido-ui';
 import { FC } from 'react';
 import { StyledBlock } from './styles';
 import { Stack } from 'shared/components';
+import {
+  ICS_ROUND,
+  ICS_ROUND_START_DATE,
+  ICS_ASSESSED_DATE,
+} from '../shared/consts';
 
 export const RoundBanner: FC = () => (
   <StyledBlock>
     <Stack direction="column">
       <Text size="sm" weight={700}>
-        ICS Application Round #2 is open
+        ICS Application Round #{ICS_ROUND} is ongoing
       </Text>
       <Text size="xxs">
-        All applications received after October 1 will be evaluated in December.
-        Stay tuned for the announcement of the final date for Application Round
-        #2.
+        All applications received after {ICS_ROUND_START_DATE} will be evaluated
+        in {ICS_ASSESSED_DATE}. Stay tuned for the announcement of the final
+        date for Application Round #{ICS_ROUND}
       </Text>
     </Stack>
   </StyledBlock>
