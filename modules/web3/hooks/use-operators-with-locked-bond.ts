87766f212edd220ae35f9564e66ed1c445e31ec6
Roman <exromany@lido.fi>
2026-01-20 15:18:01 +0400

    fix: in stealing table hide operators with amount less 10wei (#419)

--- modules/web3/hooks/use-operators-with-locked-bond.ts
+++ modules/web3/hooks/use-operators-with-locked-bond.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { STRATEGY_CONSTANT } from 'consts';
 import { useLidoSDK } from '../web3-provider';
+import { STETH_ROUNDING_THRESHOLD } from '@lidofinance/lido-csm-sdk';
 
 export const useOperatorsWithLockedBond = () => {
   const { csm } = useLidoSDK();
@@ -9,5 +10,7 @@ export const useOperatorsWithLockedBond = () => {
     queryKey: ['operators-with-locked-bond'],
     ...STRATEGY_CONSTANT,
     queryFn: () => csm.stealing.getOperatorsWithLockedBond(),
+    select: (data) =>
+      data.filter(({ locked }) => locked > STETH_ROUNDING_THRESHOLD),
   });
 };
