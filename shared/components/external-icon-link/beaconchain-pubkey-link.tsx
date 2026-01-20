a54e869473a8135c8ce3e249c61f3cc775284704
Roman <exromany@lido.fi>
2026-01-20 15:17:35 +0400

    Merge pull request #416 from lidofinance/fix/duplicates-by-cl
    fix: check duplicates by CL key status

--- shared/components/external-icon-link/beaconchain-pubkey-link.tsx
+++ shared/components/external-icon-link/beaconchain-pubkey-link.tsx
@@ -11,7 +11,6 @@ const { beaconchain } = getExternalLinks();
 
 const NON_DEPOSITED_STATUSES: KEY_STATUS[] = [
   KEY_STATUS.DEPOSITABLE,
-  KEY_STATUS.DUPLICATED,
   KEY_STATUS.INVALID,
   KEY_STATUS.NON_QUEUED,
   KEY_STATUS.UNCHECKED,
@@ -21,21 +20,17 @@ export const BeaconchainPubkeyLink: FC<
   Pick<KeyWithStatus, 'pubkey' | 'statuses'>
 > = ({ pubkey, statuses }) => {
   const href = beaconchain ? `${beaconchain}/validator/${pubkey}` : '';
-  const skip = hasInterception(statuses, NON_DEPOSITED_STATUSES);
+  const skip = !href || hasInterception(statuses, NON_DEPOSITED_STATUSES);
 
   if (skip) return null;
 
   return (
-    <>
-      {href && (
-        <MatomoLink
-          href={href}
-          title="View on beaconcha.in"
-          matomoEvent={MATOMO_CLICK_EVENTS_TYPES.beaconchainPubkeyLink}
-        >
-          <BeaconchaLink />
-        </MatomoLink>
-      )}
-    </>
+    <MatomoLink
+      href={href}
+      title="View on beaconcha.in"
+      matomoEvent={MATOMO_CLICK_EVENTS_TYPES.beaconchainPubkeyLink}
+    >
+      <BeaconchaLink />
+    </MatomoLink>
   );
 };
