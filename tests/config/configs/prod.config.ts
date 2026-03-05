bc3947e653869bbd2d398df532c8377f8e5baadf
Aleksandr <captain_sparrow@lido.fi>
2026-03-05 13:22:16 +0300

    fix: update rpcUrlToMock format in prod and testnet config files

--- tests/config/configs/prod.config.ts
+++ tests/config/configs/prod.config.ts
@@ -16,7 +16,7 @@ export class ProdConfig extends BaseConfig {
         scan: 'https://etherscan.io/',
       },
       nodeConfig: {
-        rpcUrlToMock: `**/api/rpc?chainId=1`,
+        rpcUrlToMock: [`**/api/rpc?chainId=1`],
         rpcUrl: process.env.RPC_URL as string,
         derivationPath: "m/44'/60'/0'/0",
         host: '127.0.0.1',
