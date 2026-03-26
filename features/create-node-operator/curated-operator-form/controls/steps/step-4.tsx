0e5be10ef5ceb5125c8631c5fb0b7c5eb806e3cf
exromany <exromany@lido.fi>
2026-03-26 18:59:50 +0700

    fix: disable back button while submitting new operator

--- features/create-node-operator/curated-operator-form/controls/steps/step-4.tsx
+++ features/create-node-operator/curated-operator-form/controls/steps/step-4.tsx
@@ -2,15 +2,18 @@ import { Button, Text } from '@lidofinance/lido-ui';
 import { type FC } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
 import { Stack } from 'shared/components';
+import { SubmitButtonHookForm } from 'shared/hook-form/controls';
 import type { CuratedOperatorFormInputType } from '../../context/types';
 import { VerificationSummary } from '../verification-summary';
-import { SubmitButtonHookForm } from 'shared/hook-form/controls';
 
 export const Step4: FC = () => {
   const currentStep = useWatch<CuratedOperatorFormInputType, 'step'>({
     name: 'step',
   });
-  const { setValue } = useFormContext<CuratedOperatorFormInputType>();
+  const {
+    setValue,
+    formState: { isSubmitting },
+  } = useFormContext<CuratedOperatorFormInputType>();
 
   if (currentStep !== 4) return null;
 
@@ -27,6 +30,7 @@ export const Step4: FC = () => {
           variant="outlined"
           fullwidth
           onClick={() => setValue('step', 3)}
+          disabled={isSubmitting}
         >
           Back
         </Button>
