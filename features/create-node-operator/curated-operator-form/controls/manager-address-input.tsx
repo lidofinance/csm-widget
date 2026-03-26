35dffc43a5b721095625633b51621f6d3884c908
exromany <exromany@lido.fi>
2026-03-26 18:05:21 +0700

    fix: create curated operator owner chip

--- features/create-node-operator/curated-operator-form/controls/manager-address-input.tsx
+++ features/create-node-operator/curated-operator-form/controls/manager-address-input.tsx
@@ -1,13 +1,18 @@
 import { type FC } from 'react';
 import { AddressInputHookForm } from 'shared/hook-form/controls';
 import { useCuratedOperatorFormData } from '../context';
+import { OwnerChip } from 'shared/components';
 
 export const ManagerAddressInput: FC = () => {
   const { address } = useCuratedOperatorFormData(true);
   return (
     <AddressInputHookForm
       fieldName="managerAddress"
-      label="Manager Address"
+      label={
+        <>
+          Manager Address <OwnerChip />
+        </>
+      }
       currentAddress={address}
     />
   );
