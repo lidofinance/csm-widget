69c8f99881f7894d8c2a0e49f8f2a6315eff69af
Aleksandr <captain_sparrow@lido.fi>
2026-04-06 20:05:17 -0300

    chore: update dependencies and add detailed link to welcome section

--- tests/cm-widget/pages/elements/common/element.welcomeSection.ts
+++ tests/cm-widget/pages/elements/common/element.welcomeSection.ts
@@ -6,11 +6,15 @@ export class WelcomeSection {
   navigateCMv1Section: Locator;
   connectWallet: Locator;
   navigateToCMv1Btn: Locator;
+  detailedLink: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.welcomeSection = this.page.getByTestId('welcomeSection');
     this.connectWallet = this.welcomeSection.getByTestId('connectWallet');
+    this.detailedLink = this.welcomeSection.getByRole('link', {
+      name: 'the link',
+    });
 
     this.navigateCMv1Section = this.page.getByTestId('navigateCMv1Section');
     this.navigateToCMv1Btn =
