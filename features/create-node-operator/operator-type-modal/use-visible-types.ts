2b835a07d2dc2b6d3d892cc7b4b9f5cab94d79a1
exromany <exromany@lido.fi>
2026-05-19 08:35:06 +0300

    fix: pass operator type to create page

--- features/create-node-operator/operator-type-modal/use-visible-types.ts
+++ features/create-node-operator/operator-type-modal/use-visible-types.ts
@@ -14,6 +14,7 @@ import { useShowFlags } from 'shared/hooks';
 export type VisibleType = {
   type: OPERATOR_TYPE;
   href: PATH;
+  query?: Record<string, string>;
   label: string;
   matomoEvent: MATOMO_CLICK_EVENTS_TYPES;
   primary: boolean;
@@ -35,6 +36,7 @@ const buildApplyEntry = (
     ? {
         type,
         href: PATH.CREATE,
+        query: { type: short.toLocaleLowerCase() },
         label: `Create ${short} operator`,
         matomoEvent: createEvent,
         primary: true,
