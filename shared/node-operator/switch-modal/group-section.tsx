eba229ec97422f938cef0ce7f076bb8affb38346
exromany <exromany@lido.fi>
2026-09-04 22:11:49 +0300

    fix(switch-modal): hide group View link for wallets without operator role

--- shared/node-operator/switch-modal/group-section.tsx
+++ shared/node-operator/switch-modal/group-section.tsx
@@ -10,6 +10,7 @@ import { OperatorGroupData } from './use-grouped-operators';
 import { LocalLink } from 'shared/navigate';
 import { PATH } from 'consts';
 import { formatGroupTitle } from 'shared/node-operator/utils';
+import { useShowFlags } from 'shared/hooks';
 
 type GroupSectionProps = {
   group: OperatorGroupData;
@@ -34,13 +35,14 @@ export const GroupSection: FC<GroupSectionProps> = ({
   availableMap,
   onSwitch,
 }) => {
+  const { IS_NODE_OPERATOR } = useShowFlags();
   return (
     <Stack direction="column" gap="sm">
       <Stack center spaceBetween>
         <Text size="xs" weight={700}>
           {formatGroupTitle(group)}
         </Text>
-        {group.allOperatorIds.includes(activeId) && (
+        {IS_NODE_OPERATOR && group.allOperatorIds.includes(activeId) && (
           <LocalLink href={PATH.GROUP}>
             <Button size="xs" variant="ghost">
               View
