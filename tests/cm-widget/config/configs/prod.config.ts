6e20cfda60286d3ac5dc4576f617c019e108ff1b
Aleksandr <captain_sparrow@lido.fi>
2026-09-18 19:31:44 -0300

    feat: added docker-compose for manage anvil and ipfs locally

--- tests/cm-widget/config/configs/prod.config.ts
+++ tests/cm-widget/config/configs/prod.config.ts
@@ -1,3 +1,5 @@
+import { MODULE_NAME } from '@lidofinance/lido-csm-sdk';
+import { forkPort } from 'tests/shared/config/forkPorts';
 import { BaseConfig } from './base.config';
 
 export class ProdConfig extends BaseConfig {
@@ -23,7 +25,7 @@ export class ProdConfig extends BaseConfig {
         rpcUrl: this.getRpcUrl('prod'),
         derivationPath: "m/44'/60'/0'/0",
         host: '127.0.0.1',
-        port: process.env.ANVIL_PORT ? parseInt(process.env.ANVIL_PORT) : 8545,
+        port: forkPort('mainnet', MODULE_NAME.CM),
         forkLog: {
           enabled: false,
           logToFile: true,
